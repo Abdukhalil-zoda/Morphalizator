c27bfe42bd8feaf22b1d3845294823cced0c15b7
abdu3 <abdu3@ADMIN>
2023-05-16 19:06:46 +0500

    fix

--- Morphalizator/ViewModels/DashboardViewModel.cs
+++ Morphalizator/ViewModels/DashboardViewModel.cs
@@ -1,21 +1,39 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using System;
+using System.IO;
+using System.Linq;
+using System.Windows;
 using Wpf.Ui.Common.Interfaces;
 
 namespace Morphalizator.ViewModels {
     public partial class DashboardViewModel : ObservableObject, INavigationAware {
         [ObservableProperty]
         private int _counter = 0;
-
+        [ObservableProperty]
+        public string _WordText = string.Empty;
+        [ObservableProperty]
+        public string _RootWord = string.Empty;
+        [ObservableProperty]
+        public string _RootWordType = string.Empty;
         public void OnNavigatedTo() {
         }
 
         public void OnNavigatedFrom() {
         }
 
+
+
         [RelayCommand]
-        private void OnCounterIncrement() {
-            Counter++;
+        private void Analyze() {
+            var asoses = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory,"..", "..", "..", "Assets", "asos.txt"));
+            foreach (var line in asoses) {
+                if (WordText.ToLower().Contains(line.Split()[0])) {
+                    RootWord = line.Split()[0];
+                    RootWordType = line.Split()[1];
+                    return;
+                }
+            }
         }
     }
 }
