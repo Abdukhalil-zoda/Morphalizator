b7ef48735737072dd528537ba5ca92adf9cfde90
abdu3 <abdu3@ADMIN>
2023-05-16 17:21:27 +0500

    Добавьте файлы проекта.

---
+++ Morphalizator/Views/Pages/DashboardPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Wpf.Ui.Common.Interfaces;
+
+namespace Morphalizator.Views.Pages {
+    /// <summary>
+    /// Interaction logic for DashboardPage.xaml
+    /// </summary>
+    public partial class DashboardPage : INavigableView<ViewModels.DashboardViewModel> {
+        public ViewModels.DashboardViewModel ViewModel {
+            get;
+        }
+
+        public DashboardPage(ViewModels.DashboardViewModel viewModel) {
+            ViewModel = viewModel;
+
+            InitializeComponent();
+        }
+    }
+}
---
+++ Morphalizator/Views/Pages/DataPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Wpf.Ui.Common.Interfaces;
+
+namespace Morphalizator.Views.Pages {
+    /// <summary>
+    /// Interaction logic for DataView.xaml
+    /// </summary>
+    public partial class DataPage : INavigableView<ViewModels.DataViewModel> {
+        public ViewModels.DataViewModel ViewModel {
+            get;
+        }
+
+        public DataPage(ViewModels.DataViewModel viewModel) {
+            ViewModel = viewModel;
+
+            InitializeComponent();
+        }
+    }
+}
